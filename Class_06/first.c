1a83c68615014cea5eb0716432d01d5584fa2512
FiksaisDivreiz <kristaps.locmelis1@gmail.com>
2022-09-27 09:54:57 +0300

    Class-06

---
+++ Class_06/first.c
@@ -0,0 +1,20 @@
+// vienas rindas komentārs
+// vienas rindas komentārs
+// vienas rindas komentārs
+
+/*
+komentāra paragrāfs vai bloks
+1.
+2.
+3.
+
+*/
+
+int main()//arī te var būt komentārs
+ {
+/*
+arī komentāra nokomandēts koda fragments
+if (...)
+*/
+ return 0;
+ }
